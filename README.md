46802cfaf36405ecb07cb5ff3f815c988a263bd1
Juan Pablo SG <juan.saavedra.g@mail.pucv.cl>
2023-02-08 23:47:49 -0300

    created initial project structure

---
+++ README.md
@@ -0,0 +1,7 @@
+# Requirements
+
+# Installation
+
+# Usage
+
+# Contrib
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="Nenet",
+    version="0.0.1",
+    description="A simple implementation of a neural network for educational purposes.",
+    # long_description = ...
+    author="Juan Pablo SG (jpsaavedraguerin)",
+    author_email="juanpablo@sguerin.cl",
+    url="github.com/jpsaavedraguerin/nenet",
+    # classifiers = ...
+    license="MIT"
+)
